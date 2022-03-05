cd21f3325abc2b07742348e950c0996ecb540ed0
Lemme <71924682+Lemme-lab@users.noreply.github.com>
2022-03-05 15:56:24 +0100

    added js to status symbols

--- Code/web-html/app.js
+++ Code/web-html/app.js
@@ -14,3 +14,37 @@ function changemode(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
   }
+
+  var power_img_power = document.querySelector('.power_img_power');
+  var power_img_status = document.querySelector('.power_img_status');
+  var images1 = ['PowerOn.png', 'PowerOff.png'];
+  var images2 = ['connection_off.png', 'connection_on.png'];
+  var i = 0;
+  
+  function next_power(){
+      if(i >= images1.length-1) i = -1;
+      i++;
+      return setImg1();			 
+  }
+  
+  function next_connection(){
+    if(i >= images2.length-1) i = -1;
+    i++;
+    return setImg2();			 
+}
+
+
+function setImg1(){
+  return power_img_power.setAttribute('src', "data/"+images1[i]);    
+}
+
+  function setImg2(){
+      return power_img_status.setAttribute('src', "data/"+images2[i]);    
+  }
+
+
+/*
+  const words = input.split(" ");
+  if(words[0] == "Coordinates:"){
+  document.getElementById("coordinates_text").innerHTML = words[1];
+}*/
