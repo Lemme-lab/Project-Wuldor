827e0a64f1f38fea9513d2286b2e2e8d6ef2d760
Matteo Müller <matteo.mueller@htl-klu.at>
2022-03-10 08:35:37 +0100

    slider values js

--- Code/web-html/app.js
+++ Code/web-html/app.js
@@ -22,6 +22,43 @@ function changemode(evt, cityName) {
   var images2 = ['connection_off.png', 'connection_on.png'];
   var images3 = ['check_green.png', 'check_red.png'];
   var i = 0;
+
+  var slider1 = document.getElementById("myRange1");
+  var output1 = document.getElementById("inaxis1");
+  output1.placeholder = "              AXIS 1";
+  slider1.oninput = function() {
+    output1.placeholder = this.value;
+  }
+  var slider2 = document.getElementById("myRange2");
+  var output2 = document.getElementById("inaxis2");
+  output2.placeholder = "              AXIS 2";
+  slider2.oninput = function() {
+    output2.placeholder = this.value;
+  }
+  var slider3 = document.getElementById("myRange3");
+  var output3 = document.getElementById("inaxis3");
+  output3.placeholder = "              AXIS 3";
+  slider3.oninput = function() {
+    output3.placeholder = this.value;
+  }
+  var slider4 = document.getElementById("myRange4");
+  var output4 = document.getElementById("inaxis4");
+  output4.placeholder = "              AXIS 4";
+  slider4.oninput = function() {
+    output4.placeholder = this.value;
+  }
+  var slider5 = document.getElementById("myRange5");
+  var output5 = document.getElementById("inaxis5");
+  output5.placeholder = "              AXIS 5";
+  slider5.oninput = function() {
+    output5.placeholder = this.value;
+  }
+  var slider6 = document.getElementById("myRange6");
+  var output6 = document.getElementById("inaxis6");
+  output6.placeholder = "              AXIS 6";
+  slider6.oninput = function() {
+    output6.placeholder = this.value;
+  }
   
   function next_power(){
       if(i >= images1.length-1) i = -1;
