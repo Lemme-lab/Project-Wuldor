0c8de55230f0ef9ef1864936f7648eb13c820b29
Schleichdie <Lucas.Lenarcic@gmail.com>
2022-03-21 14:13:37 +0100

    added motor programm

---
+++ Code/Microcontroller Code/Robotic-Arm-ESP32/src/Motor.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+const int dir = 0;
+const int speed = 0;
+
+void setup() {
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
--- Code/Microcontroller Code/Robotic-Arm-ESP32/src/main.cpp
+++ Code/Microcontroller Code/Robotic-Arm-ESP32/src/main.cpp
@@ -6,5 +6,5 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
