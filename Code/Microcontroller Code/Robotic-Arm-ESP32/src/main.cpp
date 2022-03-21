7cbac40440afc67d7cfaa23570d12761873b2c38
Schleichdie <Lucas.Lenarcic@gmail.com>
2022-03-21 14:53:47 +0100

    bug fix v2

--- Code/Microcontroller Code/Robotic-Arm-ESP32/src/main.cpp
+++ Code/Microcontroller Code/Robotic-Arm-ESP32/src/main.cpp
@@ -1,10 +1,17 @@
 #include <Arduino.h>
+#include <AccelStepper.h>
 
+#define dir_pin 32
+#define speed_pin 33
+#define motorInterfaceType 1
+
+AccelStepper stepper = AccelStepper(motorInterfaceType, speed_pin, dir_pin);
 
 void setup() {
- 
+  stepper.setMaxSpeed(1000);
 }
 
 void loop() {
-  
+  stepper.setSpeed(400);
+  stepper.runSpeed();
 }
