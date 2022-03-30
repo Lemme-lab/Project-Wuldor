5d686ff36cf177a6404e403bd43573ab70f5ff59
Schleichdie <Lucas.Lenarcic@gmail.com>
2022-03-30 12:47:53 +0200

    added more features to atmega mc

--- Code/Microcontroller Code/Robotic-Arm-ATMEGA328/src/main.cpp
+++ Code/Microcontroller Code/Robotic-Arm-ATMEGA328/src/main.cpp
@@ -4,12 +4,26 @@
 #include "Main_Header.h"
 #include <SPI.h>
 
+#define STATE_LOCATION 513 
+#define EEPROM_BUFFER 514 
+#define BUFFER_LENGTH 121
+
+// O -- STOP
+// H -- HOLD
+// S -- SAVE
+// R -- Read
+// D -- Drive
+
 char buf [30];
 volatile byte pos;
 volatile boolean senden;
 int motor_values[7];
+int arr_motor_value[20];
+int arr_counter = 0;
 String conv = "";
 int counter = 0;
+int last_stop = 0;
+bool hold = false;
 
 #define step1 19
 #define step2 20
@@ -58,10 +72,38 @@ void setup() {
   DDRC |= 0b00011111;
 }
 
-void loop() {
-  if(buf[pos] ==  'E'){
-    if(buf[0] == 'R'){
-      for(int i = 0; i<pos;i++){
+void driveMotors(){
+
+  for (last_stop < arr_counter || hold = false;)
+  {   
+    if(buf[0] != 'H' && buf[0] != 'S'){
+      motor1.setmoveto(arr_motor_value[motor_values[0]]);
+      motor2.setmoveto(arr_motor_value[motor_values[0]]);
+      motor3.setmoveto(arr_motor_value[motor_values[0]]);
+      motor4.setmoveto(arr_motor_value[motor_values[0]]);
+      motor5.setmoveto(arr_motor_value[motor_values[0]]);
+      motor6.setmoveto(arr_motor_value[motor_values[0]]);
+      driveMotor(motor1,motor2,motor3,motor4,motor5,motor6);
+      last_stop++
+    }   
+    else if(buf[0] != 'O'){
+      last_stop == arr_counter;
+    }
+    else if(buf[0] != 'H'){
+      hold = true;
+    }
+
+  }
+  last_stop = 0;
+}
+
+
+
+void ReadInput(){
+  if(arr_counter == 19){
+    arr_counter = 0;
+  }
+  for(int i = 0; i<pos;i++){
         if(buf[i] != ','){
           conv+=buf[i];
         }else{
@@ -69,16 +111,47 @@ void loop() {
           counter++;
         } 
       }
+      arr_motor_value[arr_counter] = motor_values;
+      arr_counter++;
+}
+
+
+
+void SaveCommands(){
+  for (size_t i = 0; i < arr_counter; i++)
+  {
+    EEPROM.write(eepromIndex, arr_motor_value[i]);
+    eepromIndex++;
+    if (eepromIndex > EEPROM_BUFFER + BUFFER_LENGTH) eepromIndex = EEPROM_BUFFER;
+    Serial.println(eepromIndex);
+    EEPROM.write(eepromIndex, 0xFF);
+  }
+  
+
+}
+
+
+
+
+
+void loop() {
+  if(buf[pos] ==  'E'){
+
+    if(buf[0] == 'O'){
+      hold = false;
+      driveMotors();
+    }
+    
+    if(buf[0] == 'R'){
+     ReadInput();
+    }
+
+    if(buf[0] == 'S'){
+      SaveCommands();
     }
 
     if(buf[0] == 'D'){
-      motor1.setmoveto(motor_values[0]);
-      motor2.setmoveto(motor_values[1]);
-      motor3.setmoveto(motor_values[2]);
-      motor4.setmoveto(motor_values[3]);
-      motor5.setmoveto(motor_values[4]);
-      motor6.setmoveto(motor_values[5]);
-      driveMotor(motor1,motor2,motor3,motor4,motor5,motor6);
+      driveMotors();
     }
   }
 }
