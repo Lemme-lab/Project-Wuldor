fef3dd33448e57352e05d18f15adec734ab0699b
Lemme <71924682+Lemme-lab@users.noreply.github.com>
2022-04-11 16:37:11 +0200

    reworked code at atmega

--- Code/Microcontroller Code/Robotic-Arm-ATMEGA328/src/motordriver.cpp
+++ Code/Microcontroller Code/Robotic-Arm-ATMEGA328/src/motordriver.cpp
@@ -2,11 +2,11 @@
 #include <AccelStepper.h>
 #include "Main_Header.h"
 
-#define nema17 1
-#define nema11_gearbox 1
-#define nema17_gearbox 1
-#define nema11 1
-#define speed_value 1000
+#define nema17 0.5555
+#define nema11_gearbox 28.333
+#define nema17_gearbox 55.5555
+#define nema11 0.5555
+#define speed_value 2000
 boolean state1 = false;
 boolean state2 = false;
 boolean state3 = false;
@@ -21,6 +21,7 @@ int get_speed(){
 }
 
 
+//this works so bad need to rework with non blocking and simultaneous code
 void driveMotor(Motor motor1, Motor motor2, Motor motor3, Motor motor4, Motor motor5, Motor motor6){
   
 int counter = 0;
@@ -44,18 +45,15 @@ AccelStepper stepper6 = AccelStepper(motor5.dir_pin, motor6.speed_pin, motor6.mo
 stepper6.setMaxSpeed(speed_value);
 stepper6.setAcceleration(50);
 
-
-
-
 Serial.print("Motor go ehhhhhh");
 
 while(state1 == false &&  state2 == false && state3 == false && state4 == false && state5 == false && state6 == false){
-stepper1.moveTo(motor1.moveto);
-stepper2.moveTo(motor2.moveto);
-stepper3.moveTo(motor3.moveto);
-stepper4.moveTo(motor4.moveto);
-stepper5.moveTo(motor5.moveto);
-stepper6.moveTo(motor6.moveto);
+stepper1.moveTo((motor1.moveto) * nema17);
+stepper2.moveTo((motor2.moveto) * nema17_gearbox);
+stepper3.moveTo((motor3.moveto) * nema17_gearbox);
+stepper4.moveTo((motor4.moveto) * nema11_gearbox);
+stepper5.moveTo((motor5.moveto) * nema11_gearbox);
+stepper6.moveTo((motor5.moveto) * nema11);
 
 if(stepper1.distanceToGo() == 0){
 stepper1.stop();
@@ -89,22 +87,22 @@ state6 = true;
 counter++;
 
 if(state1 == false){
-stepper1.run();
+stepper1.runToPosition();
 }
 if(state2 == false){
-stepper2.run();
+stepper2.runToPosition();
 }
 if(state3 == false){
-stepper3.run();
+stepper3.runToPosition();
 }
 if(state4 == false){
-stepper4.run();
+stepper4.runToPosition();
 }
 if(state5 == false){
-stepper5.run();
+stepper5.runToPosition();
 }
 if(state6 == false){
-stepper6.run();
+stepper6.runToPosition();
 }
 
 if(counter == 500000){
